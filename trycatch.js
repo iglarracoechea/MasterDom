cd8922842a88db2b82e8ce5a262ba54c3b5e7ae3
iglarracoechea <46580285+iglarracoechea@users.noreply.github.com>
2020-06-25 01:01:34 -0300

    Add files via upload

---
+++ trycatch.js
@@ -0,0 +1,10 @@
+try{
+    let year = 2020;
+    alert(year);
+
+    let arrreglo = new Array(999999999999999999)
+}catch(error){   //captura el error si hay un fallo en el codigo anterior
+   
+    console.log(error)
+    alert('Error en el codigo')
+}
